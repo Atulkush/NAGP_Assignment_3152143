108ad12176e8877eb0a40062d3a5544b2c7992a4
Atul Kushwaha <atul.kushwaha@nagarro.com>
2023-06-12 17:21:17 +0530

    Service code correction

--- Web/src/main/java/com/nagp/assignment/nagp/service/ProjectService.java
+++ Web/src/main/java/com/nagp/assignment/nagp/service/ProjectService.java
@@ -3,9 +3,11 @@ package com.nagp.assignment.nagp.service;
 import com.nagp.assignment.nagp.entity.Project;
 import com.nagp.assignment.nagp.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProjectService {
 
     @Autowired 
